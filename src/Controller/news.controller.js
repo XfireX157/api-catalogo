69d488f9249babb05a63afe70c00a3b59ae8ed83
Gustavo <gustavopereirafacal@gmail.com>
2023-02-03 17:20:47 -0300

    update

--- src/Controller/news.controller.js
+++ src/Controller/news.controller.js
@@ -85,7 +85,15 @@ export const updateID = async (req, res) => {
         const { filename, path } = req.file
         const { id } = req.params
 
-        await update( id, {filename: filename, title: title, path: path, description: description, price: price, discount: discount})
+        await update(
+            id,
+            filename,
+            title,
+            path,
+            description,
+            price,
+            discount
+        )
 
         res.status(200).json({
             messagem: "Update sucess",
