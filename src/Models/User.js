d12ac3a210d20f9034c47475c997d6379ed6fb18
gustavo <gustavopereirafacal@gmail.com>
2023-02-04 22:56:33 -0300

    escondendo a senha do usuario

--- src/Models/User.js
+++ src/Models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
-        select: true
+        select: false
     }
 }, {collection: 'users'})
 
