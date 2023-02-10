0546d724e88c784ef132445dadf51ebc2d353c03
Gustavo <gustavopereirafacal@gmail.com>
2023-02-10 16:47:14 -0300

    orden alfabetica

--- src/Services/newsService.js
+++ src/Services/newsService.js
@@ -3,7 +3,7 @@ import News from "../Models/News.js";
 export const create = (body) => News.create(body)
 export const findDocuments = () => News.countDocuments()
 export const getID = (id) => News.findById(id)
-export const findAll = () => News.find().sort({_id: -1}).populate('user', 'category')
+export const findAll = () => News.find().sort({title: 1}).populate('user', 'category')
 export const deleteID = (id) => News.findByIdAndDelete({_id: id})
 export const update = (id, updateObject) => News.findByIdAndUpdate({_id: id}, {$set: updateObject})
 export const search = (searchTerm) => News.find({title: {$regex: searchTerm, $options: 'i'}})
